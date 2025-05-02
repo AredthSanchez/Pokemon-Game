dd1c155575559accc937d3014a0055a8539ac383
Skar <146405747+Aredthsanchez@users.noreply.github.com>
2025-05-02 00:04:50 -0600

    CL3_Sanchez.java

--- CL3_Sanchez.java
+++ CL3_Sanchez.java
@@ -1,15 +1,3 @@
-/* Aredth Sanchez
-[CS1101] Comprehensive Lab 3
-This work is to be done individually. It is not permitted
-to. share, reproduce, or alter any part of this assignment
-for any purpose. Students are not permitted to share code,
-upload this assignment online in any form, or
-view/receive/ modifying code written by anyone else. This
-assignment is part of an academic course at The University
-of Texas at El Paso and a grade will be assigned for the
-work produced individually by the student.
-*/
-
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
